9e41bb811f4e68260d9bf99d6de1e5dd3d6e4e35
Christopher Wood <cmw2379@gmail.com>
2022-12-11 11:16:18 -0500

    UI tweaks

--- src/components/globalHeader/index.js
+++ src/components/globalHeader/index.js
@@ -1,19 +1,23 @@
-import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import SiteIcon from '../../images/site-icon.png';
+import Button from '@mui/material/Button';
 import './styles.scss';
 
 function GlobalHeader(props) {
 	return (
-		<header className="global-header">
-			<div className="wrapper site-header__wrapper">
-				<a href="/" className="brand">
-					<img id="site-logo" src={SiteIcon} alt="site logo" />
+		<header className='global-header'>
+			<div className='wrapper site-header__wrapper'>
+				<a href='/' className='brand'>
+					<img id='site-logo' src={SiteIcon} alt='site logo' />
 				</a>
-				<nav className="nav">
-					<IconButton onClick={props.onMenuClick}>
-						<MenuIcon size="large" sx={{ color: 'icon.primary' }} />
-					</IconButton>
+				<nav className='nav'>
+					<Button
+						onClick={props.onMenuClick}
+						endIcon={<MenuIcon size='large' sx={{ color: 'icon.primary' }} />}
+						sx={{ color: 'white', fontWeight: 'bold' }}
+					>
+						More
+					</Button>
 				</nav>
 			</div>
 		</header>
